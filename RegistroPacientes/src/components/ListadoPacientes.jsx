2f936853d7f7c03048f5b387d154832a4efcd508
Gustavo Marin <wstov@hotmail.com>
2023-04-22 15:43:24 -0600

    Componente Paciente

--- RegistroPacientes/src/components/ListadoPacientes.jsx
+++ RegistroPacientes/src/components/ListadoPacientes.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import Paciente from "./Paciente"
+
+Paciente
 
 const ListadoPacientes = () => {
     return (
-        <div className="w-1/2 lg:w-3/5">
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll ">
             <h2 className='font-black text-3xl text-center'>Se guimiento de Pacientes</h2>
+            <p className='text-xl mt-5 mb-10 text-center '>
+                Administra tus {''}
+                <span className='text-emerald-600 font-bold '>Pacientes y Citas</span>
+            </p>
 
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
 
         </div>
     )
---
+++ RegistroPacientes/src/components/Paciente.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Paciente = () => {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>e-mail: {''}
+                <span className='font-normal normal-case'>correo.correo.com</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>alta: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente
